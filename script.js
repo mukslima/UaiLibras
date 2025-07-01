7265112747287e3e8663ab433a3bf66f278336a1
mukslima <marcos.nascimento.nl@gmail.com>
2025-07-01 07:47:34 -0300

    feat: att silder + notice + news imgs

--- script.js
+++ script.js
@@ -1,40 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const images = document.querySelectorAll(".carousel-images img");
+  const slides = document.querySelectorAll(".slide");
   const prevBtn = document.querySelector(".prev");
   const nextBtn = document.querySelector(".next");
   let current = 0;
 
   function showImage(index) {
-    images.forEach((img, i) => {
-      img.classList.remove("active");
-      if (i === index) img.classList.add("active");
+    slides.forEach((slide, i) => {
+      slide.classList.remove("active");
+      if (i === index) slide.classList.add("active");
     });
   }
 
   prevBtn.addEventListener("click", () => {
-    current = (current - 1 + images.length) % images.length;
+    current = (current - 1 + slides.length) % slides.length;
     showImage(current);
   });
 
   nextBtn.addEventListener("click", () => {
-    current = (current + 1) % images.length;
+    current = (current + 1) % slides.length; 
     showImage(current);
   });
 
   showImage(current);
 });
 
-// Modal
-const modal = document.getElementById("modal");
-const abrirBtn = document.getElementById("abrirModal");
-const fecharBtn = document.querySelector(".fechar");
 
-abrirBtn.onclick = () => modal.style.display = "flex";
-fecharBtn.onclick = () => modal.style.display = "none";
-
-// Fecha modal clicando fora do conteúdo
-window.onclick = (event) => {
-  if (event.target === modal) {
-    modal.style.display = "none";
-  }
-};
